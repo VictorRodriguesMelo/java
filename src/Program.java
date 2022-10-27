4c132c9c6641b2d0788cc4724d33e5a613e27ad8
VictorRodriguesMelo <victorHugorhv@gmail.com>
2022-10-26 21:05:00 -0300

    Completo

--- src/Program.java
+++ src/Program.java
@@ -4,6 +4,7 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
+		int positivos = 0; 
 		
 		System.out.println("Digite o número de linhas: ");
 		int m = sc.nextInt();		
@@ -14,17 +15,69 @@ public class Program {
 		
 		for(int linha = 0; linha <mt.length; linha++) {
 			for(int coluna = 0; coluna < mt[linha].length; coluna++) {
-				System.out.println("...");				
+				System.out.println("Outro: ");				
 				mt [linha][coluna] = sc.nextInt();				
 			}
 		}
 		for(int linha = 0; linha < mt.length; linha++) {
 			for (int coluna = 0; coluna < mt[linha].length; coluna++) {
 				System.out.printf(mt[linha][coluna] + " ");
+				if (mt [linha] [coluna] > 0 ) {
+					positivos ++;
+				}
 		}
 			System.out.println();
-		}	
+		}		
+		System.out.println("Positivos : " + positivos);
+		System.out.println("Digite uma posição: ");
+		int entrada = sc.nextInt();
 		
+		for(int linha = 0; linha < mt.length; linha++) {
+			for (int coluna = 0; coluna < mt[linha].length; coluna++) {
+				if (mt[linha] [coluna] == entrada) {
+					if(linha > 0) {
+						//esquerdo
+						System.out.println(mt [linha - 1][coluna]);						
+					}
+						if (linha < mt.length);{
+							//direito
+							System.out.println(mt[linha + 1][coluna]);
+						}
+							if (coluna > 0) {
+								//em cima
+								System.out.println(mt[linha][coluna - 1]);
+							}
+								if (coluna < mt.length) {
+									// em baixo
+									System.out.println(mt[linha][coluna + 1]);
+								}
+								
+					
+							
+				
+				
+				
+				
+				
+				
+				
+				}
+				}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		}
+		
+		
+		
+		
+	
 		sc.close();
 	}
 
